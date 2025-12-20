5a8a76e1d4fb815a16db4b5f2b27e60a4fb74a99
abeer <abeerabdu363@gmail.com>
2025-12-20 15:36:28 +0300

    feat: add home page

--- apps/web-antd/src/api/core/user.ts
+++ apps/web-antd/src/api/core/user.ts
@@ -8,3 +8,22 @@ import { requestClient } from '#/api/request';
 export async function getUserInfoApi() {
   return requestClient.get<UserInfo>('/user/info');
 }
+export async function saveUserApi(user: UserInfo) {
+  return requestClient.post<UserInfo>('/user', user);
+}
+export async function deleteUserApi(userId: number) {
+  return requestClient.delete<boolean>(`/user/${userId}`);
+}
+
+// export async function saveUserApi(user: UserInfo) {
+//   return requestClient.put<UserInfo>('/user', user);
+// }
+
+// export async function saveUserApi(user: UserInfo) {
+//   const url = user.id ? `/user/${user.id}` : '/user/';
+//   return requestClient.request<UserInfo>(url, {
+//     data: user,
+//     // OR PUT
+//     method: user.id ? 'PUT' : 'POST',
+//   });
+// }
---
+++ apps/web-antd/src/router/routes/modules/home.ts
@@ -0,0 +1,28 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+import { $t } from '#/locales';
+
+const routes: RouteRecordRaw[] = [
+  {
+    meta: {
+      icon: 'mdi:home',
+      title: $t('page.home.title'),
+    },
+    name: 'Home',
+    path: '/home',
+    redirect: '/home/index',
+    children: [
+      {
+        name: 'HomeIndex',
+        path: '/home/index',
+        component: () => import('#/views/home/index.vue'),
+        meta: {
+          icon: 'mdi:home',
+          title: $t('page.home.index'),
+        },
+      },
+    ],
+  },
+];
+
+export default routes;
